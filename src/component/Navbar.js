e1637f3fe7d64c7af1de08971aca77bb464d2e19
piyushmehta1598 <143518463+piyushmehta1598@users.noreply.github.com>
2023-11-16 05:18:59 -0600

    Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/TextUtility-React/">
           {props.title}
         </Link>
         <button
